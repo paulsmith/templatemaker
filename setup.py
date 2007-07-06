92f7e091b44650a3043e5c4fd918431e6c418028
holovaty <holovaty@4628c333-8934-0410-b54f-c54c24ea6f33>
2007-07-06 20:18:54 +0000

    Fixed #2 -- Added 'py_modules' line to setup.py. Thanks, Paul Smith
    git-svn-id: http://templatemaker.googlecode.com/svn/trunk@8 4628c333-8934-0410-b54f-c54c24ea6f33

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     description='Given a list of text files in a similar format, templatemaker creates a template that can extract data from files in that same format.',
     author='Adrian Holovaty',
     author_email='adrian@holovaty.com',
+    py_modules=['templatemaker'],
     ext_modules=[Extension('_templatemaker', ['templatemaker.c'])],
 )
