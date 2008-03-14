f357711bbf9b533ddbcebc9e90056ecb15a818a1
Paul Smith <paulsmith@pobox.com>
2008-03-13 23:42:00 -0500

    Hard-coded version number because setup.py tries to import the module with the version number but it ultimately tries to import the as-yet non-existent C module

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-from templatemaker import __version__
 
 setup(
     name='templatemaker',
-    version=__version__,
+    version='0.0.1',
     description='Given a list of text files in a similar format, templatemaker creates a template that can extract data from files in that same format.',
     author='Adrian Holovaty',
     author_email='adrian@holovaty.com',
